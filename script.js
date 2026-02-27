1250bbfe1246215d388c7d5089cdbf51fc09fc75
244hunter244 <116167817+244hunter244@users.noreply.github.com>
2026-02-27 08:15:50 -0300

    start

---
+++ script.js
@@ -0,0 +1,20 @@
+let alunos = [
+    {
+        nm: 'arthur',
+        turma: '3C',
+        notas:[7, 3, 6, 9],
+        
+    },
+    {
+        nm: 'renan',
+        turma: '3C',
+        notas:[2, 4, 10, 7],
+    },
+    {
+        nm: 'pedro',
+        turma: '3C',
+        notas:[2, 6, 8, 7]
+    }
+]
+
+let calcular = ();
